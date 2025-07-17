a99a19b79cdf1780975a317727e68ba9e3fe85e5
ClaudioHNQ <100215157+claudiohnq@users.noreply.github.com>
2025-07-17 15:57:47 -0300

    Add files via upload

---
+++ medbot.py
@@ -0,0 +1,21 @@
+import telebot
+
+bot = telebot.TeleBot('8024320279:AAGFQKaZCwZJ7c8nMkk4HIGzYsCkNzizD6o')
+
+@bot.message_handler(func=lambda msg: msg.text.lower() == 'ajuda')
+def ajuda(msg):
+    bot.reply_to(msg, 'Aqui você encontrará um manual sobre como utilizar os seus comandos.\nVocê pode escrever os comandos sem barra!')
+
+@bot.message_handler(func=lambda msg: msg.text.lower() == 'iniciar')
+def iniciar(msg):
+    bot.reply_to(msg, 'Bem-vindo! O que você deseja fazer?\nAH: Agendar horário\nMC: Marcar compartimento')
+
+@bot.message_handler(func=lambda msg: msg.text.upper() == 'AH')
+def AH(msg):
+    bot.reply_to(msg, 'Agendamento iniciado')
+
+@bot.message_handler(func=lambda msg: msg.text.upper() == 'MC')
+def MC(msg):
+    bot.reply_to(msg, 'Muito bem, escolha o número correspondente ao compartimento desejado\n1\n2\n3\n4\n5\n6\n7')
+
+bot.infinity_polling()
