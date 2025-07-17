d5671af72ad26e1d77b3fdfd954954a88b229a4e
bruno.almd <brunoalmeida22007@gmail.com>
2025-07-17 19:41:11 -0300

    bibliotecas

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "esp_wifi.h"  // Controla a conexao do Wi-Fi do ESP32
+#include "esp_event.h" // Sistema que gerencia os eventos do ESP-IDF
+#include "nvs_flash.h" // Acesso a memoria flash persistente (NVS = Non-Volatile Storage)
+
+#include "esp_http_client.h" // Cliente HTTP para fazer as requisições, eiar os dados para o server python
+
+#include "driver/gpio.h" //Controla os pinos GPIO saidas e entradas de energia 
+#include "driver/ledc.h"  //Controla as saidas esp32 pwm
+
+#include "esp_sntp.h" //Sincroniza a hora com o servidor NTP pela internet
+#include <time.h> // Bibliioteca padrão da linguagem C para manipular o c   
